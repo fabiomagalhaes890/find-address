b386be16dc221bb93d13832183a103b3a12c2738
= <=>
2022-08-24 17:24:22 -0300

    refactor: removing folders

--- Questao1/Domain/Password.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Question1.Domain
-{
-    public class Password
-    {
-        public virtual int Value { get; set; }
-        
-        public Password(int password) => Value = password;
-    }
-}
